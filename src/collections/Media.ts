01259b9aaedbcad931f1f9441fef953e837f8328
komalkprajapati <komalprajapati230105@gmail.com>
2025-09-11 15:25:48 +0530

    commit

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -11,6 +11,11 @@ export const Media: CollectionConfig = {
       type: 'text',
       required: true,
     },
+    {
+      name: 'cloudinaryUrl',
+      type: 'text',
+      required: false,
+    },
   ],
   upload: true
 }
